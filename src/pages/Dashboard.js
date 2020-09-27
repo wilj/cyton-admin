0069612d85f656a435b9b1f3178f003f1452b113
Wil Johnson <15970051+wilj@users.noreply.github.com>
2020-09-27 19:49:08 +0000

    graphql work in progress - need to figure out why urql client reference is getting lost

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -8,7 +8,6 @@ import ChartLegend from '../components/Chart/ChartLegend'
 import PageTitle from '../components/Typography/PageTitle'
 import { ChatIcon, CartIcon, MoneyIcon, PeopleIcon } from '../icons'
 import RoundIcon from '../components/RoundIcon'
-import response from '../utils/demo/tableData'
 import {
   TableBody,
   TableContainer,
@@ -29,10 +28,30 @@ import {
   lineLegends,
 } from '../utils/demo/chartsData'
 
+import {useQuery, initClient} from 'urql'
+import gql from 'graphql-tag'
+
+// initClient({url})
+
 function Dashboard() {
   const [page, setPage] = useState(1)
-  const [data, setData] = useState([])
+  const [migrations, setMigrations] = useState([])
 
+  const [result, rexecuteQuery] = useQuery({query: gql`
+    query DashboardQuery {
+      migrations(orderBy: CREATED_TS_ASC) {
+        nodes {
+          name
+          createdTs
+        }
+      }
+    }
+  `})
+  const {data, fetching, error} = result
+
+  const response = data?.migrations?.nodes || []
+
+  
   // pagination setup
   const resultsPerPage = 10
   const totalResults = response.length
@@ -45,84 +64,33 @@ function Dashboard() {
   // on page change, load new sliced data
   // here you would make another server request for new data
   useEffect(() => {
-    setData(response.slice((page - 1) * resultsPerPage, page * resultsPerPage))
+    setMigrations(response.slice((page - 1) * resultsPerPage, page * resultsPerPage))
   }, [page])
+
+  if (error) {
+    return <div>{JSON.stringify(error)}</div>
+  }
 
   return (
     <>
       <PageTitle>Dashboard</PageTitle>
 
-      <CTA />
-
-      {/* <!-- Cards --> */}
-      <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
-        <InfoCard title="Total clients" value="6389">
-          <RoundIcon
-            icon={PeopleIcon}
-            iconColorClass="text-orange-500 dark:text-orange-100"
-            bgColorClass="bg-orange-100 dark:bg-orange-500"
-            className="mr-4"
-          />
-        </InfoCard>
-
-        <InfoCard title="Account balance" value="$ 46,760.89">
-          <RoundIcon
-            icon={MoneyIcon}
-            iconColorClass="text-green-500 dark:text-green-100"
-            bgColorClass="bg-green-100 dark:bg-green-500"
-            className="mr-4"
-          />
-        </InfoCard>
-
-        <InfoCard title="New sales" value="376">
-          <RoundIcon
-            icon={CartIcon}
-            iconColorClass="text-blue-500 dark:text-blue-100"
-            bgColorClass="bg-blue-100 dark:bg-blue-500"
-            className="mr-4"
-          />
-        </InfoCard>
-
-        <InfoCard title="Pending contacts" value="35">
-          <RoundIcon
-            icon={ChatIcon}
-            iconColorClass="text-teal-500 dark:text-teal-100"
-            bgColorClass="bg-teal-100 dark:bg-teal-500"
-            className="mr-4"
-          />
-        </InfoCard>
-      </div>
-
       <TableContainer>
         <Table>
           <TableHeader>
             <tr>
-              <TableCell>Client</TableCell>
-              <TableCell>Amount</TableCell>
-              <TableCell>Status</TableCell>
-              <TableCell>Date</TableCell>
+              <TableCell>Migration Name</TableCell>
+              <TableCell>Timestamp</TableCell>
             </tr>
           </TableHeader>
           <TableBody>
-            {data.map((user, i) => (
+            {migrations.map((mig, i) => (
               <TableRow key={i}>
                 <TableCell>
-                  <div className="flex items-center text-sm">
-                    <Avatar className="hidden mr-3 md:block" src={user.avatar} alt="User image" />
-                    <div>
-                      <p className="font-semibold">{user.name}</p>
-                      <p className="text-xs text-gray-600 dark:text-gray-400">{user.job}</p>
-                    </div>
-                  </div>
+                  <span className="text-sm">{mig.name}</span>
                 </TableCell>
                 <TableCell>
-                  <span className="text-sm">$ {user.amount}</span>
-                </TableCell>
-                <TableCell>
-                  <Badge type={user.status}>{user.status}</Badge>
-                </TableCell>
-                <TableCell>
-                  <span className="text-sm">{new Date(user.date).toLocaleDateString()}</span>
+                  <span className="text-sm">{mig.createdTs}</span>
                 </TableCell>
               </TableRow>
             ))}
@@ -137,19 +105,6 @@ function Dashboard() {
           />
         </TableFooter>
       </TableContainer>
-
-      <PageTitle>Charts</PageTitle>
-      <div className="grid gap-6 mb-8 md:grid-cols-2">
-        <ChartCard title="Revenue">
-          <Doughnut {...doughnutOptions} />
-          <ChartLegend legends={doughnutLegends} />
-        </ChartCard>
-
-        <ChartCard title="Traffic">
-          <Line {...lineOptions} />
-          <ChartLegend legends={lineLegends} />
-        </ChartCard>
-      </div>
     </>
   )
 }
